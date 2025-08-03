05744b662d2f45a7f11c954d8d8f927507a5dc03
Michael Kim <michaelhyunk@gmail.com>
2025-08-03 22:36:44 +1200

    Initial commit: Spring Boot 3 project with Gradle and Java 21

---
+++ src/main/java/com/acmeair/api/AcmeairApiApplication.java
@@ -0,0 +1,13 @@
+package com.acmeair.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmeairApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcmeairApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acmeair/api/AcmeairApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acmeair.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmeairApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
